1bff648ed01f3da55be1fb677fd7540c910f441d
peter0618 <peterchoi0618@gmail.com>
2020-07-28 13:24:06 +0900

    TOAST UI date-picker와 select-box 적용 * select-box의 위치 선정의 오류가 있어서 다시 수정해야 함...

--- src/main/frontend/src/app/ledger/ledger.component.ts
+++ src/main/frontend/src/app/ledger/ledger.component.ts
@@ -30,13 +30,29 @@ export class LedgerComponent implements OnInit {
           header: '일자',
           name: 'date',
           width: 100,
-          editor: 'text'
+          editor: {
+            type: 'datePicker',
+            options: {
+              format: 'yyyy/MM/dd'
+            }
+          }
         },
         {
           header: '항목',
           name: 'item',
           width: 150,
-          editor: 'text'
+          formatter: 'listItemText',
+          editor: {
+            type: 'select',
+            options: {
+              listItems: [
+                { text: '이월잔액', value: '00' },
+                { text: '예산입금', value: '01' },
+                { text: '예산 외 항목 지출', value: '02' },
+                { text: '찬양팀', value: '03' },
+              ],
+            },
+          }
         },
         {
           header: '비고',
@@ -82,7 +98,8 @@ export class LedgerComponent implements OnInit {
         this.rows.push({
           id,
           date: moment(stndDate).format('YYYY/MM/DD'),
-          item: itemName,
+          // item: itemName,
+          item: itemCode,
           note,
           income,
           expenditure,
@@ -92,6 +109,11 @@ export class LedgerComponent implements OnInit {
       console.log(this.rows);
 
       this.grid.resetData(this.rows);
+    });
+
+    this.grid.on('editingStart', ev => {
+      console.log('change focused cell!', ev);
+      // console.log(ev.columnName);
     });
   }
 
