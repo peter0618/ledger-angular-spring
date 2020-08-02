7f23d1e3e6c2ef752401c9d6d0ff909b880e31e4
yosephNoh <yoseph.noh@solarconnect.kr>
2020-08-03 00:36:28 +0900

    메인 페이지 추가, route 모듈 추가

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
-import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LedgerComponent} from './ledger/ledger.component';
 import {HttpClientModule} from '@angular/common/http';
-import { ToastSampleComponent } from './toast-sample/toast-sample.component';
+import {ToastSampleComponent} from './toast-sample/toast-sample.component';
+import {MainPageComponent} from './layout/main-page/main-page.component';
+import {RouteModule} from './routes/routes.module';
 
 @NgModule({
-  declarations: [AppComponent, LedgerComponent, ToastSampleComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, LedgerComponent, ToastSampleComponent, MainPageComponent],
+  imports: [BrowserModule, HttpClientModule, RouteModule],
   providers: [],
   bootstrap: [AppComponent],
 })
