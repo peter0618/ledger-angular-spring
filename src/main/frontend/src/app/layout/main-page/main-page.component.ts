d1342ec09a71c8af1a150c22870ae094f7241a0a
yosephNoh <yoseph.noh@solarconnect.kr>
2020-08-10 19:05:57 +0900

    반응형 레이아웃 추가

--- src/main/frontend/src/app/layout/main-page/main-page.component.ts
+++ src/main/frontend/src/app/layout/main-page/main-page.component.ts
@@ -6,7 +6,22 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./main-page.component.scss'],
 })
 export class MainPageComponent implements OnInit {
+  isMobileDevice: boolean;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.isMobileDevice = window.outerWidth < 780;
+  }
+
+  toggleDeviceType(){
+    this.isMobileDevice = !this.isMobileDevice;
+  }
+
+  onMobileNavBarClick(){
+    this.toggleDeviceType();
+    console.log('onMobileNavBarClick');
+    document.querySelectorAll('mobile_nav_items').forEach((ele) => {
+      console.log(ele);
+    });
+  }
 }
