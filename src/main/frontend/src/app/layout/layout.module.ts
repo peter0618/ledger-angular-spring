d70339f7aaa7f98413c411be6f11ea5b3c9a3d4a
yosephNoh <yoseph.noh@solarconnect.kr>
2020-08-11 14:39:15 +0900

    헤더 분리  login 버튼 추가

--- src/main/frontend/src/app/layout/layout.module.ts
+++ src/main/frontend/src/app/layout/layout.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {LayoutComponent} from '@app/layout/layout.component';
+import {HeaderComponent} from './header/header.component';
 
 @NgModule({
-  declarations: [LayoutComponent],
+  declarations: [LayoutComponent, HeaderComponent],
   imports: [CommonModule, RouterModule],
-  exports: [LayoutComponent]
+  exports: [LayoutComponent],
 })
 export class LayoutModule {}
